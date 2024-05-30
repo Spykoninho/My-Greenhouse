1099a17350b32e8628bb11206443157c262958eb
SpyKo_ <111909438+Spykoninho@users.noreply.github.com>
2024-05-30 13:41:04 +0200

    dockerisation

--- api/src/repository/db.js
+++ api/src/repository/db.js
@@ -1,8 +1,8 @@
-var mysql = require('mysql');
+var mysql = require('mysql2');
 var connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PWD,
+    password: process.env.DB_USER_PWD,
     database: process.env.DB_DATABASE,
     port: process.env.DB_PORT
 });
