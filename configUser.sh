1e8af3c9a7bfdb4e593ea21d05061cdc8f1ad7c4
SpyKo_ <111909438+Spykoninho@users.noreply.github.com>
2024-07-02 18:16:12 +0200

    Update configUser.sh

--- configUser.sh
+++ configUser.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 echo "Login :"
 read -p "Username or email : " USERNAME
-read -s -p "Password : " PWD; echo
-echo $API_IP_HOST/api/login
-curl -d "username=$USERNAME&password=$PWD" "$API_IP_HOST/api/login"
+read -s -p "Password : " PWD;
+response=$(curl -d -f "username=$USERNAME&password=$PWD" "$API_IP_HOST/api/login")
+jwt=$($response | jq -r '.jwt')
+if [ $jwt -eq "" ]; then
+    echo "Erreur : Impossible de récupérer le JWT depuis l'API."
+    exit 1
+fi
+
+
+sudo sed -i '/JWT=/d' /etc/environment # delete the actual var if already exist
+echo "JWT=$jwt" | sudo tee -a /etc/environment # put it in /etc/environment, tee is like >> but with the sudo right
+
+echo "JWT added, wait the reboot..."
+sleep 1
+sudo reboot
