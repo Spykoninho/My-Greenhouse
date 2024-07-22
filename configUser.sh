81b89c2293c11f281a979abe08f86baf85bd4a66
SpyKo_ <111909438+Spykoninho@users.noreply.github.com>
2024-07-22 22:29:36 +0200

    Config user (#10)
    * Update configUser.sh
    
    * configUsr

--- configUser.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Login :"
-read -p "Username or email : " USERNAME
-read -s -p "Password : " PWD; echo
-echo $API_IP_HOST/api/login
-curl -d "username=$USERNAME&password=$PWD" "$API_IP_HOST/api/login"
